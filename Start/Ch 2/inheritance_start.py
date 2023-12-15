227f2ac6a1913aa19d3c98307e6947d110e8a212
Steve Simonson <60331766+srsimonson@users.noreply.github.com>
2023-12-15 18:46:54 +0000

    done

--- Start/Ch 2/inheritance_start.py
+++ Start/Ch 2/inheritance_start.py
@@ -1,29 +1,44 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Understanding class inheritance
 
-
-class Book:
-    def __init__(self, title, author, pages, price):
+class Publication:
+    def __init__(self, title, price):
         self.title = title
         self.price = price
+
+class Periodical(Publication):
+    def __init__(self, title, price, period, publisher):
+        super().__init__(title, price)
+        self.period = period
+        self.publisher = publisher
+
+
+class Book(Publication):
+    def __init__(self, title, author, pages, price):
+        super().__init__(title, price) # This is how you initialize the super class, as in Publication
+        # self.title = title
+        # self.price = price
+        # None of the self.<whatevers> are no longer needed because they are inherited from Publication or Periodical
         self.author = author
         self.pages = pages
 
 
-class Magazine:
+class Magazine(Periodical):
     def __init__(self, title, publisher, price, period):
-        self.title = title
-        self.price = price
-        self.period = period
-        self.publisher = publisher
+        super().__init__(title, price, period, publisher)
+        # self.title = title
+        # self.price = price
+        # self.period = period
+        # self.publisher = publisher
 
-
-class Newspaper:
+class Newspaper(Periodical):
     def __init__(self, title, publisher, price, period):
-        self.title = title
-        self.price = price
-        self.period = period
-        self.publisher = publisher
+        super().__init__(title, price, period, publisher)
+
+        # self.title = title
+        # self.price = price
+        # self.period = period
+        # self.publisher = publisher
 
 
 b1 = Book("Brave New World", "Aldous Huxley", 311, 29.0)
